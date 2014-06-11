6f17f27055b01af1b594a407f5e3a10b0bef0d99
Nelson da Costa <ndacosta@eleven-labs.com>
2014-06-11 17:04:12 +0200

    added travis params and bootstrap steps

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,17 @@
 <?php
 
-if (!is_file($autoloadFile = __DIR__ . '/../vendor/autoload.php')) {
-    throw new \LogicException('Could not find autoload.php in vendor/');
+error_reporting(-1);
+date_default_timezone_set('UTC');
+
+if (!file_exists('~/.aws/credentials')) {
+    die('~/.aws/credentials does not exist');
 }
 
-require $autoloadFile;
+// Ensure that composer has installed all dependencies
+if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
+    die("Dependencies must be installed using composer:\n\nphp composer.phar install\n\n"
+        . "See http://getcomposer.org for help with installing composer\n");
+}
+
+// Include the composer autoloader
+require dirname(__DIR__).'/../vendor/autoload.php';
